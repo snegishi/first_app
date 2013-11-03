8b7295acf3d34e0373d7a882f8b454c71b0ad5a5
Shin Negishi <negishi@streamreceiver.com>
2013-11-04 02:24:52 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '71a707054453a87223c51c35ef2b0802c347ce0ba8cf1581b9eeaa9396f8fa617993c780fff52ef7efa0e410f0bdf449ded90b1a766a851efc4c63e3002a0ae3'
